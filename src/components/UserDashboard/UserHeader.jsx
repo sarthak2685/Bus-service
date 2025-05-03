19f622b8c2725c1a4e92a8d4cf637d29c77e3e82
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2025-05-03 12:34:42 +0530

    login part done (#12)

--- src/components/UserDashboard/UserHeader.jsx
+++ src/components/UserDashboard/UserHeader.jsx
@@ -4,15 +4,15 @@ import { FaBars } from "react-icons/fa";
 // const token = S?.data?.token;
 
 const UserHeader = ({ toggleSidebar }) => {
-    // const user = JSON.parse(localStorage.getItem("user")) || {};
-    // // if user is null then redirect to home page
-    // if (!user.data.token && user.data.type !== "admin") {
-    //     window.location.href = "/";
-    // }
+    const user = JSON.parse(localStorage.getItem("user")) || {};
+    // if user is null then redirect to home page
+    if (!user.data.token && user.data.type !== "admin") {
+        window.location.href = "/";
+    }
 
-    // const firstLetter = user.data.type
-    //     ? user.data.type.charAt(0).toUpperCase()
-    //     : "S";
+    const firstLetter = user.data.user_data?.name
+        ? user.data.user_data.name.charAt(0).toUpperCase()
+        : "S";
 
     return (
         <header className="bg-white shadow-lg flex items-center justify-between p-4 relative">
@@ -31,7 +31,7 @@ const UserHeader = ({ toggleSidebar }) => {
                 {/* User Info */}
                 <div className="flex items-center space-x-4">
                     <div className="w-10 h-10 rounded-full bg-blue-600 text-white flex items-center justify-center text-lg font-bold shadow-md">
-                        {/* {firstLetter} */}
+                        {firstLetter}
                     </div>
                 </div>
             </div>
