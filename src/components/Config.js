12f574be783d27b5e8fa589f0190e87ff609d3a0
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2025-06-21 12:34:56 +0530

    Razorpay implementation completed (#17)

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,6 +1,8 @@
 const config = {
     // apiUrl: 'https://buss.pythonanywhere.com/api',
     apiUrl: 'https://api.capitalbusservice.in/api',
-    };
-    
-    export default config;
+    razorpayKey: 'rzp_live_VxoX03VmMRDU6n',
+    razorpaySecret: 'gP4QKS19FXTOWVgdzajahVUt'
+};
+
+export default config;
